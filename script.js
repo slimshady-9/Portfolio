5d9267bcf25d52263a9933d58d59dbc608fd4f11
Ketan Sarda <kksarda9@gmail.com>
2024-07-14 00:13:49 +0530

    Adding modal, project details

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function openModal(header, body) {
+  const iframeContainer = document.getElementById('iframeContainer');
+  const iframe = document.getElementById('modalIframe');
+  iframeContainer.style.display = 'flex';
+
+  // Set the src and load the iframe
+  iframe.src = 'modal.html';
+  
+  iframe.onload = function() {
+    iframe.contentWindow.postMessage({ header: header, body: body }, '*');
+  };
+}
+
+window.addEventListener('click', function(event) {
+  const iframeContainer = document.getElementById('iframeContainer');
+  if (event.target === iframeContainer) {
+    iframeContainer.style.display = 'none';
+  }
+});
